d1e5353b495bfb02c5dd0a04fe16566b2e5c8ada
pingjui <pingjuiliao@gmail.com>
2019-05-05 18:24:03 -0700

    add linked_crashes script

---
+++ cromulence/link_crashes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import os
+import sys
+from shutil import rmtree
+
+def is_one_of_the_crash(filename) :
+    return filename[0:3] == "id:"
+
+def main() :
+    if not os.path.isdir("o") :
+        print "fuzzing result not found !"
+        quit()
+
+    if not os.path.isdir("o/crashes") :
+        print "fuzzing result not found !"
+        quit()
+
+    srcs = [ f for f in os.listdir("o/crashes") if is_one_of_the_crash(f) ]
+    print srcs
+
+    if len(srcs) < 0 :
+        print "no crashes file to symlink"
+        quit()
+
+    target_dir = "./linked_crashes"
+    if os.path.exists(target_dir) :
+        rmtree(target_dir)
+
+    os.mkdir(target_dir)
+
+    for src in srcs :
+        src_path = "../o/crashes/" + src
+
+        dst_path = target_dir + "/" + "id" + str(int(src[3:9]))
+        os.symlink(src_path, dst_path)
+
+
+
+
+
+
+
+
+if __name__ == '__main__' :
+    main()
